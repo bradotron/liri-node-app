d71c3883ee853304c23e850071f4b5d9534a6b3d
Bradley Jensen <bradley13@gmail.com>
2019-02-23 10:28:36 -0500

    initial functionality of spotify API

--- liri.js
+++ liri.js
@@ -1,3 +1,27 @@
-// 
+// add packages
+// load a .env that holds API keys into process.env
 require("dotenv").config();
 
+// using the node-spotify-api module
+var Spotify = require('node-spotify-api');
+
+// using Axios for web API calls
+var axios = require(`axios`);
+
+// store your spotify API keys in this file
+var keys = require(`./keys`) ;
+
+console.log("packages loaded");
+
+// spotify testing
+
+var spotify = new Spotify(keys.spotify);
+ 
+spotify
+  .search({ type: 'track', query: 'All the Small Things' })
+  .then(function(response) {
+    console.log(response);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
