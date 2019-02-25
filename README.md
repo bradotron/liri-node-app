46c08e9ba27e00318f397e2a34bf9c068b1c3814
Bradley Jensen <bradley13@gmail.com>
2019-02-24 20:04:14 -0500

    completed basic requirements

--- README.md
+++ README.md
@@ -1,1 +1,87 @@
-# liri-node-app
+# Welcome to the ReadMe for Brad's LIRI App
+This goal of this app is to interface with three API's, so the user can search for movies, bands, or songs. The user will interface with the app on the command line.
+
+## How to use the App
+The user interfaces with the app via the command line. The user passes a command and a search term as arguments. There are four possible commands for the app: 
+
+1. "concert-this"
+2. "spotify-this-song"
+3. "movie-this"
+4. "do-what-it-says"
+
+  * example command and output
+  * $ node liri movie-this "The Terminator"
+    packages loaded
+    Results powered by OMDB Web API
+    Title: The Terminator
+    Year: 1984
+    Internet Movie Database rated this: 8.0/10
+    Rotten Tomatoes rated this: 100%
+    Metacritic rated this: 84/100
+    Produced in: UK, USA
+    Language: English, Spanish
+    Plot: A seemingly indestructible android is sent from 2029 to 1984 to assassinate a waitress, whose unborn son will lead humanity in a war against the machines, while a soldier from that war is sent to protect her at all costs.
+    Actors: Arnold Schwarzenegger, Michael Biehn, Linda Hamilton, Paul Winfield
+
+## Commands in Detail
+
+1. "concert-this"
+  * This command will search for any upcoming events for the band name that is passed as a second argument. 
+
+  * If no band name is passed, this function will default to "Taylor Swift"
+
+  * Example: 
+  $ node liri concert-this "Red Hot Chili Peppers"
+  searching for concerts by: Red+Hot+Chili+Peppers
+  Event 1
+  Venue: Brisbane Entertainment Centre
+  Location: Boondall, Australia
+  Date: 02/25/2019
+
+  * Note: For band names that are multiple words, you must put double quotes around the band name. Refer to the example for proper syntax.
+
+2. "spotify-this-song"
+  * This command will search for the song that is passed as argument two to the function. This function uses the spotify API, with the Tracks endpoint.
+
+  * If no song name is passed, this function will default to "The Sign"
+
+  * Example: 
+  $ node liri spotify-this-song "Shake It Off"
+  Results powered by Spotify API
+  Artist: Taylor Swift
+  Song Title: Shake It Off
+  Preview Link: https://open.spotify.com/track/5xTtaWoae3wi06K5WfVUUH
+  Album: 1989
+
+  * Note: For song names that are multiple words, you must put double quotes around the song name. Refer to the example for proper syntax.
+
+3. "movie-this"
+  * This command will search for a movie with the title that is passed as the second argument. The function will return the first search result from the omdbAPI.
+
+  * If no movie name is passed, this function will default to "Mr. Nobody"
+
+  * Example: 
+  $ node liri movie-this "this is the end"
+  Results powered by OMDB Web API
+  Title: This Is the End
+  Year: 2013
+  Internet Movie Database rated this: 6.6/10
+  Rotten Tomatoes rated this: 83%
+  Metacritic rated this: 67/100
+  Produced in: USA
+  Language: English, Spanish
+  Plot: 6 Los Angeles celebrities are stuck in James Franco's house after a series of devastating events just destroyed the city. Inside, the group not only will have to face with the apocalypse, but with themselves.
+  Actors: James Franco, Jonah Hill, Seth Rogen, Jay Baruchel
+
+  * Note: For movie names that are multiple words, you must put double quotes around the movie name. Refer to the example for proper syntax.
+
+4. "do-what-it-says"
+  * This command will read in the file "random.txt" from the same directory that you execute liri.js from. Each line in "random.txt" will contain one of the previous three commands and arguments. Each line will be formatted as follows:
+    * command-one,"search-term"
+
+  * The function is designed to execute as many commands as are in the file.
+
+
+
+
+
--- liri.js
+++ liri.js
@@ -8,24 +8,60 @@ var Spotify = require("node-spotify-api");
 // using Axios for web API calls
 var axios = require(`axios`);
 
+// using momentjs to manipulate the date and time
+var moment = require(`moment`);
+
+var fs = require(`fs`);
+
 // store your spotify API keys in this file
 var keys = require(`./keys`);
 
-console.log("packages loaded");
-
-var arg1 = process.argv[2];
-var arg2 = process.argv[3];
-
-// hardcoded args for testing
-//arg1 = "movie-this";
-//arg2 = "Terminator";
-
-// this app can take in four commands:
 // 1. `concert-this`
 //    this command will search the bands-in-town API for an artist and output the following information for each event:
 //      1. name of the venue
 //      2. venue location
 //      3. date of the event (use moment format MM/DD/YYY)
+// var queryUrl = "https://rest.bandsintown.com/artists/" + searchName + "/events?app_id=codingbootcamp";
+var concertThis = function(band) {
+  if (band) {
+    // a band name was passed
+    console.log("good name");
+  } else {
+    // no band name
+    band = "Metallica";
+  }
+
+  band = band.replace(/ /g, `%20`);
+  let url = `https://rest.bandsintown.com/artists/${band}/events?app_id=codingbootcamp`;
+
+  axios
+    .get(url)
+    .then(function(response) {
+      console.log("Concert Results Powered by BandsInTown");
+      if (response.data.length > 0) {
+        for (let i = 0; i < response.data.length; i++) {
+          console.log(`Event ${i + 1}`);
+          // log the name of the venue
+          console.log(`Venue: ${response.data[i].venue.name}`);
+          // log the venue location: city, country
+          console.log(
+            `Location: ${response.data[i].venue.city}, ${
+              response.data[i].venue.country
+            }`
+          );
+          // log the date of the event
+          console.log(
+            `Date: ${moment(response.data[i].datetime).format("MM/DD/YYYY")}`
+          );
+        }
+      } else {
+        console.log("No events found for that name.");
+      }
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+};
 
 // 2. `spotify-this-song`
 //    this will search for a song and output:
@@ -36,7 +72,7 @@ var arg2 = process.argv[3];
 //    * if there is no song provided, default search is for "The Sign" by Ace of Base
 var spotifyThis = function(song) {
   let spotify = new Spotify(keys.spotify);
-  // check the song argument 
+  // check the song argument
   if (song) {
     // song is not undefined, just search for it
     //console.log("true");
@@ -48,17 +84,18 @@ var spotifyThis = function(song) {
   spotify
     .search({ type: "track", query: song })
     .then(function(response) {
+      console.log(`Results powered by Spotify API`);
       let myTrack = response.tracks.items[0];
       // log the Artist(s)
-      for(let i=0; i<myTrack.artists.length; i++) {
+      for (let i = 0; i < myTrack.artists.length; i++) {
         console.log(`Artist: ${myTrack.artists[i].name}`);
-      };
+      }
       // log the song's name
       console.log(`Song Title: ${myTrack.name}`);
       // log a preview link to the song on Spotify
       console.log(`Preview Link: ${myTrack.external_urls.spotify}`);
       // log the album
-      console.log(`Album: ${myTrack.album.name}`)
+      console.log(`Album: ${myTrack.album.name}`);
     })
     .catch(function(err) {
       console.log(err);
@@ -86,64 +123,95 @@ let movieThis = function(movie) {
     movie = "Mr. Nobody";
   }
 
-  let url = `http://www.omdbapi.com/?apikey=${keys.omdb.apiKey}&s=${movie.replace(/ /g, `+`)}`;
-  axios.get(url)
-  .then(function (response) {
-    // console.log(response);
-    if(!response.data) {
-      console.log("Movie not found, try searching again...");
-    } else {
-      // good response, log out the stuff
-      let myMovie = response.data.Search[0];
-      axios.get(`http://www.omdbapi.com/?apikey=${keys.omdb.apiKey}&t=${myMovie.Title.replace(/ /g, `+`)}`)
-      .then(function(response) {
-        //console.log(response.data);
-        let myMovie = response.data;
-        console.log(`Title: ${myMovie.Title}`);
-        console.log(`Year: ${myMovie.Year}`);
-        for(let i=0; i<myMovie.Ratings.length; i++) {
-          console.log(`${myMovie.Ratings[i].Source} rated this: ${myMovie.Ratings[i].Value}`);
-        }
-        console.log(`Produced in: ${myMovie.Country}`);
-        console.log(`Language: ${myMovie.Language}`);
-        console.log(`Plot: ${myMovie.Plot}`);
-        console.log(`Actors: ${myMovie.Actors}`);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    }
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-}
+  let url = `http://www.omdbapi.com/?apikey=${
+    keys.omdb.apiKey
+  }&s=${movie.replace(/ /g, `+`)}`;
+  axios
+    .get(url)
+    .then(function(response) {
+      console.log(`Results powered by OMDB Web API`);
+      // console.log(response);
+      if (!response.data) {
+        console.log("Movie not found, try searching again...");
+      } else {
+        // good response, log out the stuff
+        let myMovie = response.data.Search[0];
+        axios
+          .get(
+            `http://www.omdbapi.com/?apikey=${
+              keys.omdb.apiKey
+            }&t=${myMovie.Title.replace(/ /g, `+`)}`
+          )
+          .then(function(response) {
+            //console.log(response.data);
+            let myMovie = response.data;
+            console.log(`Title: ${myMovie.Title}`);
+            console.log(`Year: ${myMovie.Year}`);
+            for (let i = 0; i < myMovie.Ratings.length; i++) {
+              console.log(
+                `${myMovie.Ratings[i].Source} rated this: ${
+                  myMovie.Ratings[i].Value
+                }`
+              );
+            }
+            console.log(`Produced in: ${myMovie.Country}`);
+            console.log(`Language: ${myMovie.Language}`);
+            console.log(`Plot: ${myMovie.Plot}`);
+            console.log(`Actors: ${myMovie.Actors}`);
+          })
+          .catch(function(error) {
+            console.log(error);
+          });
+      }
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+};
 
 // 4. `do-what-it-says`
 //    This will use the 'fs' node package to read in from random.txt and execute that command
-
-switch (arg1) {
-  case "concert-this":
-    console.log(`searching for concerts by: ${arg2}`);
-    break;
-
-  case "spotify-this-song":
-    console.log(`Results powered by Spotify API`);
-    spotifyThis(arg2);
-    break;
-
-  case "movie-this":
-    console.log(`Results powered by OMDB Web API`);
-    movieThis(arg2);
-    break;
-
-  case "do-what-it-says":
+var doWhatItSays = function() {
+  // read the file and execute that command
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    if (error) {
+      return console.log(error);
+    }
     console.log(`reading random.txt for commands`);
 
-    break;
+    data.split(`\r\n`).forEach(function(element) {
+      if (element.length > 0) {
+        let args = element.split(`,`);
+        processArgs(args[0], args[1]);
+      }
+    });
+  });
+};
 
-  default:
-    console.log(
-      `Unrecognized command, options are: "concert-this", "spotify-this-song", "movie-this", or "do-what-it-says"`
-    );
-}
+var processArgs = function(arg1, arg2) {
+  switch (arg1) {
+    case "concert-this":
+      concertThis(arg2);
+      break;
+
+    case "spotify-this-song":
+      spotifyThis(arg2);
+      break;
+
+    case "movie-this":
+      movieThis(arg2);
+      break;
+
+    case "do-what-it-says":
+      doWhatItSays();
+      break;
+
+    default:
+      console.log(
+        `Unrecognized command, options are: "concert-this", "spotify-this-song", "movie-this", or "do-what-it-says"`
+      );
+  }
+};
+
+// main code execution here
+processArgs(process.argv[2], process.argv[3]);
